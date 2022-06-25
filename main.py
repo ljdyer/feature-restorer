6fc3acf149e52cab44ab853c96bb63fa7eb9bda7
Laurence Dyer <ljdyer@gmail.com>
2022-06-25 07:04:25 +0100

    aa

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ Assumptions:
 
 import re
 import sympy
+import pandas as pd
 
 class SampleMaker:
 
+    # ====================
     def __init__(self,
                  data: list,
                  capitalisation: bool,
@@ -35,6 +37,7 @@ class SampleMaker:
         self.prime_to_feature_char = {p: c for c, p in self.feature_char_to_prime.items()}
         self.generate_char_maps()
 
+    # ====================
     def generate_char_maps(self):
 
         all_chars = set()
@@ -52,21 +55,52 @@ class SampleMaker:
         self.char_to_int = {c: i for c, i in zip(all_chars, range(len(all_chars)))}
         self.int_to_char = {i: c for c, i in self.char_to_int.items()}
 
-    def datapoint_to_samples(self, datapoint: str) -> list:
+    # ====================
+    def datapoint_to_samples(self, datapoint_index: int) -> list:
 
-        print(datapoint)
-        return datapoint
+        datapoint = self.data[datapoint_index]
+        if self.spaces:
+            print(datapoint)
+            print(type(datapoint))
+            words = datapoint.split()
+            try:
+                substrs = []
+            except:
+                print('Huh?')
+                quit()
+            for i in range(len(words)):
+                try:
+                    substrs.append(' '.join(words[i:]))
+                except:
+                    print(words[i])
+                    quit()
+            # substrs = [ for i in range(len(words))]
+            substrs = [s for s in substrs
+                       if len(re.sub(rf"[{''.join(self.feature_chars)}]", '', s)) > self.seq_length]
+            samples = []
+            for substr in substrs:
+                this_sample = self.substr_to_sample(substr)
+                if this_sample:
+                    samples.append(this_sample)
+            return samples
+        else:
+            raise ValueError('Not implemented yet when spaces=False')
 
+    # ====================
     def substr_to_sample(self, substr: str) -> tuple:
 
         output_chars = []
         class_list = []
         chars = list(substr)
-        # Substring can't begin with a feature char
         if chars[0] in self.feature_chars:
+            # Substring can't begin with a feature char
             return None
-        for _ in self.seq_length:
-            this_char = chars.pop(0)
+        for _ in range(self.seq_length):
+            try:
+                this_char = chars.pop(0)
+            except IndexError:
+                # Substr not long enough
+                return None
             if self.capitalisation:
                 output_chars.append(self.get_int_from_char(this_char.lower()))
                 class_list.append(1 if this_char.islower() else 2)
@@ -75,16 +109,23 @@ class SampleMaker:
                 class_list.append(1)
             while chars[0] in self.feature_chars:
                 this_feature_char = chars.pop(0)
-                print(class_list)
-                print(type(class_list))
                 class_list[-1] *= self.feature_char_to_prime[this_feature_char]
         assert len(output_chars) == self.seq_length
         assert len(class_list) == self.seq_length
         return (output_chars, class_list)
 
+    # ====================
     def get_int_from_char(self, char: str) -> int:
 
         if char in self.char_to_int:
             return self.char_to_int[char]
         else:
             return self.char_to_int['<UNK>']
+
+
+if __name__ == "__main__":
+
+    data_path = 'TED_TRAIN.csv'
+    data = pd.read_csv(data_path)['all_cleaned'].to_list()
+    sample_maker = SampleMaker(data, capitalisation=True, spaces=True, other_features=list('.,'), seq_length=100)
+    print(sample_maker.datapoint_to_samples(0))
