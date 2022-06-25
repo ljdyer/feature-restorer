f4a25eb02ab4139442b497bb3f4e811a4ed2d813
Laurence Dyer <ljdyer@gmail.com>
2022-06-25 06:44:19 +0100

    a

--- main.py
+++ main.py
@@ -56,32 +56,6 @@ class SampleMaker:
 
         print(datapoint)
         return datapoint
-        # if self.spaces:
-        #     print(datapoint)
-        #     print(type(datapoint))
-        #     words = datapoint.split()
-        #     try:
-        #         substrs = []
-        #     except:
-        #         print('Huh?')
-        #         quit()
-        #     for i in range(len(words)):
-        #         try:
-        #             substrs.append(' '.join(words[i:]))
-        #         except:
-        #             print(words[i])
-        #             quit()
-        #     # substrs = [ for i in range(len(words))]
-        #     substrs = [s for s in substrs
-        #                if len(re.sub(rf"[{''.join(self.feature_chars)}]", '', s)) > self.seq_length]
-        #     samples = []
-        #     for substr in substrs:
-        #         this_sample = self.substr_to_sample(substr)
-        #         if this_sample:
-        #             samples.append(this_sample)
-        #     return samples
-        # else:
-        #     raise ValueError('Not implemented yet when spaces=False')
 
     def substr_to_sample(self, substr: str) -> tuple:
 
